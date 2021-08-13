daca6040a3a7cca2bb0ad11612ad2cba85705343
ihsan <42092819+ihsanfil@users.noreply.github.com>
2021-08-13 14:54:57 +0300

    veri erişim sınıfları oluşturuldu

---
+++ AFirmasi.MyNotes.Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using AFirmasi.Core.Entities;
+using System.Collections.Generic;
+
+namespace AFirmasi.MyNotes.Entities
+{
+    public class Category: IEntity //1 kategori birden fazla not tarafından kullanılabilir dedik
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public List<Note> Notes { get; set; }
+    }
+
+
+}
---
+++ AFirmasi.MyNotes.Entities/Note.cs
@@ -0,0 +1,16 @@
+﻿using AFirmasi.Core.Entities;
+
+namespace AFirmasi.MyNotes.Entities
+{
+    public class Note:IEntity //1 notun 1 kategorisi olabilir dedik 
+    {
+        public int Id { get; set; }
+        public string NoteTitle { get; set; }
+        public string NoteDescription { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+
+
+}
